30fe2f357f2edf100c54c55c8b7d4f59427f6998
Besedin Eugene <bee@crtweb.ru>
2022-09-08 11:36:28 +0700

    deprecated code deleted

--- app/routers/docs_metadata.py
+++ app/routers/docs_metadata.py
@@ -11,41 +11,4 @@ tags_metadata = [
         "name": "Admin",
         "description": "Административная панель, модерация пользователей, создание новых",
     },
-    {
-        "name": "Topic",
-        "description": "crud для тематик"
-    },
-    {
-        "name": "Program",
-        "description": "crud для програм"
-    },
-    {
-        "name": "Session",
-        "description": "crud для сессий, базовая логика"
-    },
-    {
-        "name": "Tag",
-        "description": "создание, получение тэгов, базовая логика"
-    },
-    {
-        "name": "Assistance",
-        "description": "Администрирование подсказок к сессиям"
-    },
-    {
-        "name": "ChatReplics",
-        "description": "Создание/удаление реплик чата и получение всех реплик по сессии"
-    },
-    {
-        "name": "Recommendations",
-        "description": "Логика администрирования рекоммендаций"
-    },
-    {
-        "name": "OnboardingQuestions",
-        "description": "Неполная логика администрирования вопросов для рекоммендаций"
-    },
-    {
-        "name": "Locale",
-        "description": "Изменение или добавление перевода для какого-либо объекта. Прямого "
-                       "удаления нет",
-    }
 ]
